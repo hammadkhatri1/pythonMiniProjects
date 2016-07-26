0cad3a3e9f0de0b28cc0071e9ad519d1eca7e017
hammad khatri <hammad khatri>
2016-07-27 00:54:47 +0500

    day 1

---
+++ calc1.py
@@ -0,0 +1,7 @@
+type=input("addition or subtraction?")
+print (type ," Calculator")
+val1=input("any first number?")
+val2=input("any second number?")
+
+val3=val1+val2
+print ("the value is ",(val3))
